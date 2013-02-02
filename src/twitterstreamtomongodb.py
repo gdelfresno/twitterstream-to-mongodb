8b1f2f5a5d1b7d927cd1ad506cad778dfc836df1
Michael Markieta <michael@spatialanalysis.ca>
2013-02-01 19:47:32 -0500

    Update src/twitterstreamtomongodb.py

--- src/twitterstreamtomongodb.py
+++ src/twitterstreamtomongodb.py
@@ -1,92 +1,92 @@
 '''
 Twitter Stream To MongoDB (c) by gdelfresno
- 
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
- 
+
 import threading
 import datetime
 import time
 import re
- 
+
 from optparse import OptionParser
 from pymongo import Connection
- 
+
 from ssl import SSLError
- 
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.utils import import_simplejson
- 
+
 json = import_simplejson()
- 
+
 active_terms = {}
- 
+
 STREAM_URL = "https://stream.twitter.com/1/statuses/filter.json?track=%s"
- 
- 
+
+
 def get_parser():
     parser = OptionParser()
     parser.add_option("-d", "--database", dest="database", help="mongodb database name")
     parser.add_option("-s", "--server", dest="server", help="mongodb host")
     parser.add_option("-o", "--oauth", dest="oauthfile", help="file with oauth options")
-    parser.add_option("-f", "--file", dest="file", help="terms file")
-    #parser.add_option("-a", "--all_keys", dest="all_keys", help="MongoDB receives all keys from json")
-    #parser.add_option("-u", "--use_keys", dest="use_keys", help="MongoDB receieves subset of keys from json")
-    parser.add_option("-r", "--retweets", dest="retweets", help="specify whether or not retweets are added to the database")
+    parser.add_option("-t", "--track", dest="track", help="track terms file")
+    #parser.add_option("-a", "--all_keys", dest="all_keys", help="MongoDB receives all keys from json (True/False)")
+    #parser.add_option("-u", "--use_keys", dest="use_keys", help="MongoDB receieves subset of keys from json (list)")
+    parser.add_option("-r", "--retweets", dest="retweets", help="Allow retweets into MongoDB")
     parser.usage = "bad parametres"
     return parser
- 
- 
+
+
 def updateSearchQuery(options):
     print 'Updating terms: %s' % datetime.datetime.now()
- 
+
     query = ",".join(active_terms.keys())
- 
+
     try:
         streamThread.stopConsume()
- 
+
     except:
         pass
- 
+
     streamThread = StreamConsumerThreadClass(query, options.oauthfile)
     streamThread.setDaemon(True)
     
     streamThread.start()
- 
- 
+
+
 def addTerm(term):
     if term == '':
         return
- 
+
     active_terms[term] = term
- 
- 
+
+
 def deleteTerm(term):
     if term == '':
         return
- 
+
     active_terms.pop(term)
- 
- 
+
+
 def updateTerms(options):
     
     fileterms = []
     
     update = False
-    f = file(options.file, 'r')
+    f = file(options.track, 'r')
     for line in f.readlines():
         term = str.strip(line) 
         
@@ -105,23 +105,23 @@ def updateTerms(options):
      
     if update:
         updateSearchQuery(options)
- 
- 
+
+
 def prettyPrintStatus(status):
     text = status["text"]
     description = status['user']['screen_name']
     if "retweeted_status" in status:
         description = ("%s RT by %s") % (status["retweeted_status"]["user"]["screen_name"], status['user']['screen_name'])
         text = status["retweeted_status"]["text"]
- 
+
     try:
         return '[%s][%-36s]: %s' % (status['created_at'], description, text)
     except UnicodeEncodeError:
         return "Can't decode UNICODE tweet"
     except :
         return "Error printing status"
- 
- 
+
+
 class MongoDBCoordinator:
     def __init__(self, host='localhost', database='TwitterStream'):
         try:
@@ -132,8 +132,8 @@ class MongoDBCoordinator:
         
         self.db = self.mongo[database]
         self.tuits = {}
- 
- 
+
+
     def addTuit(self, tweet):
         for term in active_terms.keys():
             
@@ -144,20 +144,20 @@ class MongoDBCoordinator:
 
             strre = re.compile(term, re.IGNORECASE)
             match = strre.search(content)
-            
+
             if match:
                 if not term in self.db.collection_names():
                     self.db.create_collection(term)
-                
+
                 collection = self.db[term]
                 collection.save(tweet)
-        
+
                 try:
                     print "[%-15s]%s" % (term, prettyPrintStatus(tweet))           
                 except Exception as (e):
                     print "Error %s" % e.message
- 
- 
+
+
 class MongoDBListener(StreamListener):
     """
     A listener handles tweets are the received from the stream. 
@@ -166,7 +166,7 @@ class MongoDBListener(StreamListener):
     def on_data(self, data):
         """
         Called when raw data is received from connection.
- 
+
         Override this method if you wish to manually handle
         the stream data. Return False to stop stream and close connection.
         """
@@ -176,29 +176,29 @@ class MongoDBListener(StreamListener):
             else:
                 jstatus = json.loads(data)
                 mongo.addTuit(jstatus)
-        
+
         elif 'in_reply_to_status_id' in data:
             jstatus = json.loads(data)
             mongo.addTuit(jstatus)
-            
+
         elif 'delete' in data:
             delete = json.loads(data)['delete']['status']
             if self.on_delete(delete['id'], delete['user_id']) is False:
                 return False
- 
+
         elif 'limit' in data:
             if self.on_limit(json.loads(data)['limit']['track']) is False:
                 return False
- 
- 
+
+
     def on_error(self, status):
         print status
- 
- 
+
+
     def on_limit(self, track):
         print "###### LIMIT ERROR #######"
- 
- 
+
+
 #Class that track the stream
 class StreamConsumerThreadClass(threading.Thread):
     def __init__(self, term='', oauthfile=''):
@@ -214,12 +214,12 @@ class StreamConsumerThreadClass(threading.Thread):
         auth.set_access_token(oauth['access_token'], oauth['access_token_secret'])
     
         self.stream = Stream(auth, listener, timeout=60)  
- 
- 
+
+
     def stopConsume(self):
         self.stream.disconnect()
- 
- 
+
+
     def run(self):
         now = datetime.datetime.now()
         print "Twitter Stream with terms: %s started at: %s" % (self.getName(), now)
@@ -234,8 +234,8 @@ class StreamConsumerThreadClass(threading.Thread):
             except SSLError, e:
                 print e
                 connected = False            
- 
- 
+
+
 if __name__ == "__main__":
     parser = get_parser()
     (options, args) = parser.parse_args()
@@ -248,7 +248,7 @@ if __name__ == "__main__":
         while True:
             updateTerms(options)
             time.sleep(5)
- 
+
     except KeyboardInterrupt, e:
         print "Closing stream"
         streamThread.stopConsume()
